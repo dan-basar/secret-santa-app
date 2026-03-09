021c4903987497600043cc27f98c98e3e703911c
Dan Basar <dan.basar@outlook.com>
2026-03-09 15:41:42 -0400

    changed authentication to u/p

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,14 +3,8 @@ import sql from 'mssql';
 const config: sql.config = {
   server: process.env.AZURE_SQL_SERVER!,
   database: process.env.AZURE_SQL_DATABASE!,
-  authentication: {
-    type: 'azure-active-directory-service-principal-secret',
-    options: {
-      clientId: process.env.AZURE_CLIENT_ID!,
-      clientSecret: process.env.AZURE_CLIENT_SECRET!,
-      tenantId: process.env.AZURE_TENANT_ID!,
-    },
-  },
+  user: process.env.AZURE_SQL_USER!,
+  password: process.env.AZURE_SQL_PASSWORD!,
   options: {
     encrypt: true,
     trustServerCertificate: false,
